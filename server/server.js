a581338e11f337fdf116b7a4423f34e72e595711
hee <zcgy432@gmail.com>
2025-09-12 18:13:34 +0900

    2025-09-12

---
+++ server/server.js
@@ -0,0 +1,161 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const oracledb = require('oracledb');
+
+const app = express();
+app.use(cors());
+
+// ejs 설정
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '.')); // .은 경로
+
+const config = {
+  user: 'SYSTEM',
+  password: 'test1234',
+  connectString: 'localhost:1521/xe'
+};
+
+// Oracle 데이터베이스와 연결을 유지하기 위한 전역 변수
+let connection;
+
+// 데이터베이스 연결 설정
+async function initializeDatabase() {
+  try {
+    connection = await oracledb.getConnection(config);
+    console.log('Successfully connected to Oracle database');
+  } catch (err) {
+    console.error('Error connecting to Oracle database', err);
+  }
+}
+
+initializeDatabase();
+
+// 엔드포인트
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.get('/emp/list', async (req, res) => {
+  const { deptNo } = req.query;
+
+  let query ="";
+  if(deptNo !="" && deptNo !=null){
+    query+=`WHERE E.DEPTNO = ${deptNo} `
+  }
+  try {
+    const result = await connection.execute(`SELECT * FROM EMP E INNER JOIN DEPT D ON E.DEPTNO = D.DEPTNO `+ query + `ORDER BY SAL DESC`);
+    const columnNames = result.metaData.map(column => column.name);
+    // 쿼리 결과를 JSON 형태로 변환
+    const rows = result.rows.map(row => {
+      // 각 행의 데이터를 컬럼명에 맞게 매핑하여 JSON 객체로 변환
+      const obj = {};
+      columnNames.forEach((columnName, index) => {
+        obj[columnName] = row[index];
+      });
+      return obj;
+    });
+    res.json({
+        result : "success",
+        list : rows
+    });
+  } catch (error) {
+    console.error('Error executing query', error);
+    res.status(500).send('Error executing query');
+  }
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.get('/login', async (req, res) => {
+  const { userId , password } = req.query;
+
+  try {
+    const result = await connection.execute(`SELECT * FROM TB_USER WHERE USERID = '${userId}' AND PASSWORD = '${password}'`);
+    const columnNames = result.metaData.map(column => column.name);
+    // 쿼리 결과를 JSON 형태로 변환
+    const rows = result.rows.map(row => {
+      // 각 행의 데이터를 컬럼명에 맞게 매핑하여 JSON 객체로 변환
+      const obj = {};
+      columnNames.forEach((columnName, index) => {
+        obj[columnName] = row[index];
+      });
+      return obj;
+    });
+    res.json({
+        result : "success",
+        info:rows
+    });
+  } catch (error) {
+    console.error('Error executing query', error);
+    res.status(500).send('Error executing query');
+  }
+});
+
+
+app.get('/user/insert', async (req, res) => {
+  const { userId, email , password , name , birth , phone , gender,athority } = req.query;
+
+  try {
+    await connection.execute(
+      `INSERT INTO TB_USER VALUES (:userId , :password , :name , :birth , :phone , :email , :gender , SYSDATE ,:athority )`,
+      [userId, password, name,birth,phone,email,gender,athority],
+      { autoCommit: true }
+    );
+    res.json({
+        result : "success"
+    });
+  } catch (error) {
+    console.error('Error executing insert', error);
+    res.status(500).send('Error executing insert');
+  }
+});
+
+app.get('/emp/delete', async (req, res) => {
+  const { empNo } = req.query;
+  try {
+    await connection.execute(
+      `DELETE FROM EMP WHERE EMPNO = :empNo`,
+      [empNo],
+      { autoCommit: true }
+    );
+    res.json({
+        result : "success"
+    });
+  } catch (error) {
+    console.error('Error executing insert', error);
+    res.status(500).send('Error executing insert');
+  }
+});
+
+app.get('/user/check', async (req, res) => {
+  const { userId } = req.query;
+
+  try {
+    const result = await connection.execute(`SELECT * FROM TB_USER WHERE USERID = '${userId}'`);
+    const columnNames = result.metaData.map(column => column.name);
+    // 쿼리 결과를 JSON 형태로 변환
+    const rows = result.rows.map(row => {
+      // 각 행의 데이터를 컬럼명에 맞게 매핑하여 JSON 객체로 변환
+      const obj = {};
+      columnNames.forEach((columnName, index) => {
+        obj[columnName] = row[index];
+      });
+      return obj;
+    });
+    res.json({
+        result : "success",
+        list : rows
+    });
+  } catch (error) {
+    console.error('Error executing query', error);
+    res.status(500).send('Error executing query');
+  }
+});
+
+// 서버 시작
+app.listen(3009, () => {
+  console.log('Server is running on port 3009');
+});
